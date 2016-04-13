1e97de60cac719f4102ce1e38f8f7c52c7f14611
melemary <melemary@vt.edu>
2016-04-12 20:48:46 -0400

    Added files via upload
    updated slightly

--- SongList.java
+++ SongList.java
@@ -1,12 +1,31 @@
 package songvisualizer;
 
+import java.util.Iterator;
+
 public class SongList {
     
-    private Person[] personArray;
-    private Song[] songArray;
     private LinkedList<Song> songList;
+    private Iterator<Song> iter;
     
-    public SongList(Person[] personArray, Song[] songArray) {
-        
+    public SongList(LinkedList<Song> songList) {
+        this.songList = songList;
+        iter = songList.iterator();
+    }
+    
+    public void sortBy(String sortCriteria)
+    {
+        switch (sortCriteria) {
+            case "artist":
+                while (iter.hasNext())
+                {
+                    iter.next().getArtist();
+                }
+            case "genre":
+                something
+            case "year":
+                something
+            default: //title
+                something
+        }
     }
 }
